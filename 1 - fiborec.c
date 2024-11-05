6a43132a735a09fe655032c2dbfaae4fe14d17c1
higor <silhigor.freitas@gmail.com>
2024-11-05 20:06:11 -0300

    Commit Commited

---
+++ 1 - fiborec.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>  // gettimeofday
+
+int callOrder=0;
+
+void indent(int n) {
+  for (int i=0;i<n;i++) {
+    putchar('\t');
+  }
+}
+
+long int fibonaccir(long int n, int nivel) {
+	indent(nivel);
+	printf ("%02d - Entrou no cálculo de F(%ld). \n", ++callOrder, n);
+	if(n <= 1) {
+		return n;
+	} else {
+		int aux1=fibonaccir(n-1, nivel+1);
+		indent(nivel);
+		printf ("%02d - Retornou ao cálculo de F(%ld). \n", ++callOrder, n);
+		int aux2=fibonaccir(n-2, nivel+1);
+		indent(nivel);
+		printf ("%02d - Retornou ao cálculo de F(%ld). \n", ++callOrder, n);
+		return ( aux1 + aux2);
+	}
+}
+
+void main (void) {
+    struct timeval begin, end;
+    gettimeofday(&begin, 0);
+
+    printf ("%ld \n", fibonaccir(25,0));
+
+    gettimeofday(&end, 0);
+
+    long seconds = end.tv_sec - begin.tv_sec;
+    long microseconds = end.tv_usec - begin.tv_usec;
+    long elapsed = seconds*1e6 + microseconds;
+    printf("\n HEREEEE %ld\t \n", elapsed);
+}
---
+++ 2 - fibomemo.c
@@ -0,0 +1,48 @@
+/*memoized fibo, still recursive*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>  // gettimeofday
+
+int callOrder=0;
+
+void indent(int n) {
+  for (int i=0;i<n;i++) {
+    putchar('\t');
+  }
+}
+
+void fibonaccir(long int n, int nivel, long int *f, long int *f1) {
+	//indent(nivel);
+	//printf ("%02d - Entrou no cálculo de F(%ld). \n", ++callOrder, n);
+	if(n <= 1) {
+		*f=1;
+		*f1=1;
+		return;
+	} else {
+		long int g, g1;
+		fibonaccir(n-1, nivel+1, &g, &g1);
+		*f=g+g1;
+		*f1=g;
+		//indent(nivel);
+		//printf ("%02d - Retornou ao cálculo de F(%ld). \n", ++callOrder, n);
+		return;
+	}
+}
+
+void main (void) {
+    long int f, f1;
+
+    struct timeval begin, end;
+    gettimeofday(&begin, 0);
+
+    fibonaccir(5,0, &f, &f1);
+    printf ("%ld \n", f);
+
+    gettimeofday(&end, 0);
+
+    long seconds = end.tv_sec - begin.tv_sec;
+    long microseconds = end.tv_usec - begin.tv_usec;
+    long elapsed = seconds*1e6 + microseconds;
+    printf("%ld\t ", elapsed);
+}
